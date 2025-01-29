e9b54e4220d3001281f0aebe27ba1009353c7d35
Amr-Safwat <amrsafwat02@gmail.com>
2025-01-29 11:33:07 +0200

    created a hero section

--- src/components/2-hero/hero.jsx
+++ src/components/2-hero/hero.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import './hero.css'
+import React from "react";
+import "./hero.css";
 
 export default function hero() {
   return (
-    <div>
-      Hero
-    </div>
-  )
+    <section className="hero flex">
+      <div className="left-section">
+        <div className="avatar">
+          <img src="./Amr.jpg" alt="" />
+          <i className="fa-solid fa-circle-check check-icon"></i>
+        </div>
+        <h1>Front End Developer</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente ea
+          molestias iste pariatur velit natus. Iusto natus voluptas accusantium!
+          Asperiores cupiditate officia est nam voluptates aliquid dolor.
+        </p>
+        <div className="icons">
+          <a href="">
+            <i className="fa-brands fa-facebook"></i>
+          </a>
+          <a href="">
+            <i className="fa-brands fa-github"></i>
+          </a>
+          <a href="">
+            <i className="fa-brands fa-instagram"></i>
+          </a>
+          <a href="">
+            <i className="fa-brands fa-linkedin"></i>
+          </a>
+        </div>
+      </div>
+
+      <div className="right-section border">right</div>
+    </section>
+  );
 }
