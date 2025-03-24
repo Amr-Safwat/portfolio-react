2e125ec41c940587ca850015af7d93c00b32ebbf
Amr-Safwat <amrsafwat02@gmail.com>
2025-03-24 11:32:41 +0200

    build left section

--- src/components/3-main/main.jsx
+++ src/components/3-main/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
 import './main.css'
 
 export default function main() {
   return (
-    <div>
-      Main
-    </div>
+    <main className='flex'>
+      <section className='border left-section'>
+        <ul>
+          <li className='active'>All projects</li>
+          <li>HTML&CSS</li>
+          <li>JaveScript</li>
+          <li>React</li>
+        </ul>
+      </section>
+      <section className='border right-section'>left sec</section>
+    </main>
   )
 }
