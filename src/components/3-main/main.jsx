1cc630431681a87919ac61cf610dbb2e3fc7c39f
Amr-Safwat <amrsafwat02@gmail.com>
2025-03-28 02:58:13 +0200

    Projects Department was established

--- src/components/3-main/main.jsx
+++ src/components/3-main/main.jsx
@@ -3,7 +3,7 @@ import './main.css'
 export default function main() {
   return (
     <main className='flex'>
-      <section className='border left-section'>
+      <section className='left-section'>
         <ul>
           <li className='active'>All projects</li>
           <li>HTML&CSS</li>
@@ -11,7 +11,41 @@ export default function main() {
           <li>React</li>
         </ul>
       </section>
-      <section className='border right-section'>left sec</section>
+      <section className='right-section'>
+        <article className='card'>
+          <img src="/public/work2.png" alt="" />
+          <div className="box">
+            <div className="title">landing Page</div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam vel officiis provident dicta sit</p>
+            <div className="links">
+              <i className="fa-brands fa-github"></i>
+              <a href="">more</a>
+            </div>
+          </div>
+        </article>
+        <article className='card'>
+          <img src="/public/work2.png" alt="" />
+          <div className="box">
+            <div className="title">landing Page</div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam vel officiis provident dicta sit</p>
+            <div className="links">
+              <i className="fa-brands fa-github"></i>
+              <a href="">more</a>
+            </div>
+          </div>
+        </article>
+        <article className='card'>
+          <img src="/public/work2.png" alt="" />
+          <div className="box">
+            <div className="title">landing Page</div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam vel officiis provident dicta sit</p>
+            <div className="links">
+              <i className="fa-brands fa-github"></i>
+              <a href="">more</a>
+            </div>
+          </div>
+        </article>
+      </section>
     </main>
   )
 }
